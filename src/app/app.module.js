258e1560f5e4662784d686d5f012fe97cc0ae84a
LockonF <zurielovsko@gmail.com>
2015-12-13 22:57:01 -0600

    Resueltos errores de Frontend en Persona/Registro

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,7 +4,7 @@
         .module('app', [
             'triangular','flow','ngFileUpload','ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngMaterial',
             'ui.router','pascalprecht.translate', 'LocalStorageModule', 'googlechart', 'chart.js', 'linkify', 'ui.calendar', 'angularMoment', 'textAngular', 'uiGmapgoogle-maps', 'hljs', 'md.data.table', angularDragula(angular),
-            'restangular','toastr',
+            'restangular','toastr','ngMdIcons',
             // 'seed-module'
             // uncomment above to activate the example seed module
             //'app.examples',
