8ae8e5ab2d205d11ca6dc550a87c11e34874f9b6
LockonF <zurielovsko@gmail.com>
2015-12-21 21:25:39 -0600

    Agregada posibilidad de actualizar datos en el formulario de Proyecto/Registro. Cambiado labels del Wizard a la especificación de Edgar

--- src/app/mainApp/proyectos/controllers/registrarProyectos.controller.js
+++ src/app/mainApp/proyectos/controllers/registrarProyectos.controller.js
@@ -6,25 +6,98 @@
         .controller('registrarProyectoController', registrarProyectoController);
 
     /* @ngInject */
-    function registrarProyectoController(toastr,Restangular,$state) {
+    function registrarProyectoController($scope,toastr,Restangular,$state) {
 
         var vm = this;
-
         activate();
+        vm.steps= [
+            'PROJECT.REGISTER.PROJECT_SELECT',
+            'PROJECT.REGISTER.INFO',
+            'PROJECT.REGISTER.BACKGROUND',
+            'PROJECT.REGISTER.JUSTIFICATION',
+            'PROJECT.REGISTER.OBJECTIVES',
+            'PROJECT.REGISTER.REACH'];
+        vm.proyecto = {
+            "id": null,
+            "Titulo": null,
+            "Descripcion": null,
+            "Antecedentes": null,
+            "Justificacion": null,
+            "Objetivos": null,
+            "Alcances": null,
+            "created_at": null,
+            "updated_at": null
+        };
 
-        vm.steps= ['Información','Antecedentes','Justificación','Objetivos','Alcances'];
-        vm.proyecto = null;
         vm.isOrganizacion = false;
         vm.organizaciones = null;
         vm.loadOrganizations  = loadOrganizations;
         vm.saveProject = saveProject
 
 
+        vm.isNewProject         = true;
+        vm.firstStepDisabled    = true;
+
+        vm.projectList          = null;
+        vm.selectedProject      = null;
+        vm.getProyecto          = getProyecto;
+
+        vm.proyectoLabel = 'PROJECT.REGISTER.NEW_PROJECT';
+        vm.changeSwitch         = changeSwitch;
+
+
+
+
+        function getProyecto()
+        {
+            Restangular.all('Proyecto').one('Persona',vm.selectedProject).customGET().then(function(res){
+                vm.proyecto = res;
+                vm.proyectoLabel = vm.proyecto.Titulo;
+            })
+
+        }
+
+
+        function changeSwitch()
+        {
+            if(vm.isNewProject)
+            {
+                vm.proyecto         = {
+                    "id": null,
+                    "Titulo": null,
+                    "Descripcion": null,
+                    "Antecedentes": null,
+                    "Justificacion": null,
+                    "Objetivos": null,
+                    "Alcances": null,
+                    "created_at": null,
+                    "updated_at": null,
+                    "pivot": {
+                        "idPersona": null,
+                        "idProyecto": null,
+                        "Owner": null
+                    }
+                };
+                vm.proyectoLabel    = 'PROJECT.REGISTER.NEW_PROJECT';
+                vm.selectedProject  = null;
+                $scope.projectSelect.$setPristine();
+            }
+            else{
+                vm.proyectoLabel    = 'PROJECT.REGISTER.SELECTING_PROJECT';
+                vm.selectedProject  = null;
+                $scope.projectSelect.$setPristine();
+            }
+        }
+
+
         function activate()
         {
             Restangular.all('Persona').customGET().then(function(res)
             {
-
+                //Ruta
+                Restangular.all('Proyecto').all('Persona').customGET().then(function(res){
+                    vm.projectList = res.Proyectos;
+                })
             }).catch(function(err){
                 $state.go('triangular.admin-default.personas_registro');
                 toastr.error('Debe de haber una persona registrada para acceder a este módulo','Error');
@@ -51,12 +124,30 @@
 
             if(!vm.isOrganizacion)
             {
-                Restangular.all('Proyecto').all('Persona').customPOST(vm.proyecto).then(function(res){
-                    toastr.success('Se han guardado correctamente los datos','Éxito')
-                }).catch(function(err){
-                    toastr.error('Hubo un error al guardar los datos','Error')
-                })
+                if(vm.proyecto.id==null){
+                    Restangular.all('Proyecto').all('Persona').customPOST(vm.proyecto).then(function(res){
+                        toastr.success('Se han guardado correctamente los datos','Éxito')
+                        vm.proyecto=res;
+                        vm.proyectoLabel = vm.proyecto.Titulo;
+
+                    }).catch(function(err){
+                        toastr.error('Hubo un error al guardar los datos','Error')
+                    })
+                }
+                else{
+                    Restangular.all('Proyecto').one('Persona',vm.proyecto.id).customPUT(vm.proyecto).then(function(res){
+                        toastr.success('Se han actualizado correctamente los datos','Éxito')
+                        vm.proyectoLabel = vm.proyecto.Titulo;
+                    }).catch(function(err){
+                        toastr.error('Hubo un error al actualizar los datos','Error')
+                    })
+                }
+                Restangular.all('Proyecto').all('Persona').customGET().then(function(res){
+                    vm.projectList = res.Proyectos;
+                });
+
             }
+
 
 
         }
