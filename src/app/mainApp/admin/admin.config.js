d3393e9b28d6fcf76903a5dfcda96ea23821852f
Santiago Sinisterra <santiago.sinisterra.s@gmail.com>
2016-01-24 17:42:28 -0600

    .editorconfig

--- src/app/mainApp/admin/admin.config.js
+++ src/app/mainApp/admin/admin.config.js
@@ -1,45 +1,42 @@
-/**
- * Created by lockonDaniel on 10/14/15.
- */
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app.mainApp.admin')
-        .config(moduleConfig);
+  angular
+    .module('app.mainApp.admin')
+    .config(moduleConfig);
 
-    /* @ngInject */
-    function moduleConfig($translatePartialLoaderProvider, $stateProvider) {
-        $translatePartialLoaderProvider.addPart('app/mainApp/admin');
+  /* @ngInject */
+  function moduleConfig($translatePartialLoaderProvider, $stateProvider) {
+    $translatePartialLoaderProvider.addPart('app/mainApp/admin');
 
-        $stateProvider
-            .state('triangular.admin-default.admin', {
-                // set the url of this page
-                url: '/admin',
-                // set the html template to show on this page
-                templateUrl: 'app/mainApp/admin/index.tmpl.html',
-                // set the controller to load for this page
-                controller: 'indexAdminController',
-                controllerAs: 'vm',
-                data: {
-                    layout: {
-                        toolbarSize: 'default',
-                        toolbarShrink: true,
-                        toolbarClass: 'none',
-                        contentClass: '',
-                        sideMenuSize: 'full',
-                        footer: true
-                    },
-                    requireLogin: true,
-                    requirePrivileges:'Supervisor',
-                    redirect:'auth.login'
+    $stateProvider
+      .state('triangular.admin-default.admin', {
+        // set the url of this page
+        url: '/admin',
+        // set the html template to show on this page
+        templateUrl: 'app/mainApp/admin/index.tmpl.html',
+        // set the controller to load for this page
+        controller: 'indexAdminController',
+        controllerAs: 'vm',
+        data: {
+          layout: {
+            toolbarSize: 'default',
+            toolbarShrink: true,
+            toolbarClass: 'none',
+            contentClass: '',
+            sideMenuSize: 'full',
+            footer: true
+          },
+          requireLogin: true,
+          requirePrivileges: 'Supervisor',
+          redirect: 'auth.login'
 
-                }
+        }
 
-            });
-
-
+      });
 
 
-    }
+
+
+  }
 })();
