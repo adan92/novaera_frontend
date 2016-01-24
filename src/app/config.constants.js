46d24cccc6ae55bb0ba585c1e802c0e23adfd735
Santiago Sinisterra <santiago.sinisterra.s@gmail.com>
2016-01-24 17:13:19 -0600

    excluir dependencias y tmp del control de versiones, config personal para API

--- src/app/config.constants.js
+++ src/app/config.constants.js
@@ -6,14 +6,14 @@
         .constant('ROUTES',{
 
             //DANIEL
-            //FILE_ROUTE:'http://homestead.app/',
-            //API_ROUTE: 'http://homestead.app/api/'
+            FILE_ROUTE:'http://homestead.app/',
+            API_ROUTE: 'http://homestead.app/api/'
 
 
 
             //Virtualizado
-            FILE_ROUTE:'http://127.0.0.1:8888/novaera_laravel/public/',
-            API_ROUTE: 'http://127.0.0.1:8888/novaera_laravel/public/api/'
+            // FILE_ROUTE:'http://127.0.0.1:8888/novaera_laravel/public/',
+            // API_ROUTE: 'http://127.0.0.1:8888/novaera_laravel/public/api/'
             //Equipo
             /*
             FILE_ROUTE:'http://127.0.0.1:8000/files/',
