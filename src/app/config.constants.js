752440ab787066ce6b2e75115da2a0b93e5d0e96
LockonF <zurielovsko@gmail.com>
2016-01-29 22:13:01 -0600

    Agregado JTW Helper para verificar si el token expiró.
    Agregado refresh del token para que la sesión no se muera si no se utilizó durante una hora.
    
    Cambiado authService, authIncerceptorService

--- src/app/config.constants.js
+++ src/app/config.constants.js
@@ -9,11 +9,21 @@
             //FILE_ROUTE:'http://127.0.0.1:8888/novaera_laravel/public/',
             //API_ROUTE: 'http://127.0.0.1:8888/novaera_laravel/public/api/'
             //Vagrant
-            
             //FILE_ROUTE:'http://189.189.108.60:8888/novaera_laravel/public/',
             //API_ROUTE: 'http://189.189.108.60:8888/novaera_laravel/public/api/'
 
             //Virtualizado
+           // FILE_ROUTE:'http://homestead.app/',
+           // API_ROUTE: 'http://homestead.app/api/'
+
+
+            //Virtualizado
+            FILE_ROUTE:'http://192.168.10.10/',
+            API_ROUTE: 'http://192.168.10.10/api/'
+
+            //FILE_ROUTE:'http://189.189.108.60:8888/novaera_laravel/public/',
+            //API_ROUTE: 'http://189.189.108.60:8888/novaera_laravel/public/api/'
+
             //FILE_ROUTE:'http://148.204.58.168/novaera_laravel/public/',
             //API_ROUTE: 'http://148.204.58.168/novaera_laravel/public/api/'
             //Equipo
@@ -21,7 +31,5 @@
             FILE_ROUTE:'http://127.0.0.1:8000/files/',
             API_ROUTE: 'http://127.0.0.1:8000/api/'
              */
-             FILE_ROUTE:'http://localhost:8000/files/',
-             API_ROUTE: 'http://localhost:8000/api/'
         });
 })()
