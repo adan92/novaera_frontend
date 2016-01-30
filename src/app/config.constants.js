d09fdcd6322574e66120a3cd174f0a06440d8a01
LockonF <zurielovsko@gmail.com>
2016-01-29 22:13:01 -0600

    Agregado JTW Helper para verificar si el token expiró.
    Agregado refresh del token para que la sesión no se muera si no se utilizó durante una hora.
    
    Cambiado authService, authIncerceptorService

--- src/app/config.constants.js
+++ src/app/config.constants.js
@@ -15,6 +15,9 @@
             FILE_ROUTE:'http://192.168.10.10/',
             API_ROUTE:'http://192.168.10.10/api/'
 
+           // FILE_ROUTE:'http://homestead.app/'
+           // API_ROUTE: 'http://homestead.app/api/'
+
 
             //Virtualizado
             //FILE_ROUTE:'http://148.204.58.168/novaera_laravel/public/',
@@ -24,6 +27,8 @@
 
             //FILE_ROUTE:'http://189.189.108.60:8888/novaera_laravel/public/',
             //API_ROUTE: 'http://189.189.108.60:8888/novaera_laravel/public/api/'
+            //FILE_ROUTE:'http://148.204.58.168/novaera_laravel/public/',
+            //API_ROUTE: 'http://148.204.58.168/novaera_laravel/public/api/'
             //Equipo
             /*
             FILE_ROUTE:'http://127.0.0.1:8000/files/',
