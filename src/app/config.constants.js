5cdd705c2dfe0f6f37835cec57f85089a832a2c2
christian <amezcua92@hotmail.com>
2016-01-30 00:20:42 -0600

    Se agrego la selección del tipo de perfil del usuario, ademas se creo el servicio de profile  donde podre obtener el tipo de perfil que va usar el usuario dentro de la aplicación.

--- src/app/config.constants.js
+++ src/app/config.constants.js
@@ -12,8 +12,11 @@
 
 
             //Virtualizado
-            FILE_ROUTE:'http://127.0.0.1:8888/novaera_laravel/public/',
-            API_ROUTE: 'http://127.0.0.1:8888/novaera_laravel/public/api/'
+            FILE_ROUTE:'http://192.168.10.10/',
+            API_ROUTE: 'http://192.168.10.10/api/'
+
+            //FILE_ROUTE:'http://189.189.108.60:8888/novaera_laravel/public/',
+            //API_ROUTE: 'http://189.189.108.60:8888/novaera_laravel/public/api/'
             //Equipo
             /*
             FILE_ROUTE:'http://127.0.0.1:8000/files/',
