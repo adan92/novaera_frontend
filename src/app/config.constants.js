9f4312e085ec33b051559e5f3d67129b30e33a63
Santiago Sinisterra <santiago.sinisterra.s@gmail.com>
2016-02-02 19:23:22 -0600

    configurar el proxy de gulp para dejar de tener dolores de cabeza con el CORS...

--- src/app/config.constants.js
+++ src/app/config.constants.js
@@ -1,18 +1,25 @@
 (function() {
-    'use strict';
+  'use strict';
 
-    angular
-        .module('app')
-        .constant('ROUTES',{
+  angular
+    .module('app')
+    .constant('ROUTES', {
 
-            //DANIEL 
-            //FILE_ROUTE:'http://127.0.0.1:8888/novaera_laravel/public/',
-            //API_ROUTE: 'http://127.0.0.1:8888/novaera_laravel/public/api/'
-            //Vagrant
-            //FILE_ROUTE:'http://189.189.108.60:8888/novaera_laravel/public/',
-            //API_ROUTE: 'http://189.189.108.60:8888/novaera_laravel/public/api/'
- 
-            FILE_ROUTE:'http://192.168.10.10/',
-            API_ROUTE:'http://192.168.10.10/api/' 
-        });
+      FILE_ROUTE: '/',
+      API_ROUTE: 'api/'
+
+      //FILE_ROUTE:'http://127.0.0.1:8888/novaera_laravel/public/',
+      //API_ROUTE: 'http://127.0.0.1:8888/novaera_laravel/public/api/'
+      //Vagrant
+      //FILE_ROUTE:'http://189.189.108.60:8888/novaera_laravel/public/',
+      //API_ROUTE: 'http://189.189.108.60:8888/novaera_laravel/public/api/'
+
+      //Virtualizado
+      // FILE_ROUTE:'http://homestead.app/'
+      //FILE_ROUTE:'http://homestead.app/',
+      //API_ROUTE: 'http://homestead.app/api/'
+      //Homestead windows
+      //   FILE_ROUTE: 'http://192.168.10.10/',
+      // API_ROUTE: 'http://192.168.10.10/api/'
+    });
 })();
