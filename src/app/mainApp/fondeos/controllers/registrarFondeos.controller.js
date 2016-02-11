db4a976c87e55f5b49227e5eb65532f89d61bdb6
DarkXavier <pacoshola07@hotmail.com>
2016-02-11 00:45:15 -0600

    corregido controlador Registro Fondeos

--- src/app/mainApp/fondeos/controllers/registrarFondeos.controller.js
+++ src/app/mainApp/fondeos/controllers/registrarFondeos.controller.js
@@ -41,7 +41,8 @@
         // Registro de programa de fondeo
         function registrarfondeo() {
             console.log("Entrando a la funcion");
-            if (vm.fondeo.id === null) {
+            console.log(vm.fondeo)
+            if (vm.fondeo.id == null) {
                 console.log("Ya entre");
                 var promise = Fondeo.crearFondeo(vm.fondeo);
                 promise.then(function(res){
@@ -53,20 +54,32 @@
                 });
             }
             else {
-                console.log("No entre");
-                var promise = Proyecto.updateProject(vm.proyecto);
+                console.log("Estoy editando");
+                var promise = Fondeo.updateFondeo(vm.fondeo);
                 promise.then(function(res){
                     toastr.success(vm.successText, vm.successUpdateText);
-                    vm.proyectoLabel = vm.proyecto.Titulo;
+                    vm.fondeoLabel = vm.fondeo.Titulo;
                 }).catch(function(err){
                     toastr.error(vm.failureText, vm.failureStoreText);
                 });
             }
-            var promise = Proyecto.getAllProjects();
+            console.log("Estoy Mostrando");
+            var promise = Fondeo.getAllFondeos();
             promise.then(function (value) {
-                vm.projectList = value;
+                vm.Fondeos = value;
             });
 
+            vm.sureText = Translate.translate('DIALOGS.YOU_SURE');
+            vm.acceptText = Translate.translate('DIALOGS.ACCEPT');
+            vm.cancelText = Translate.translate('DIALOGS.CANCEL');
+            vm.dialogText = Translate.translate('DIALOGS.WARNING');
+            vm.successText = Translate.translate('DIALOGS.SUCCESS');
+            vm.successStoreText = Translate.translate('DIALOGS.SUCCESS_STORE');
+            vm.successUpdateText = Translate.translate('DIALOGS.SUCCESS_UPDATE');
+            vm.successDeleteText = Translate.translate('DIALOGS.SUCCESS_DELETE');
+            vm.failureText = Translate.translate('DIALOGS.FAILURE');
+            vm.failureStoreText = Translate.translate('DIALOGS.FAIL_STORE');
+            vm.failureDeleteText = Translate.translate('DIALOGS.FAIL_DELETE');
         }
 
 
