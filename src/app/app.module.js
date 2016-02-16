48e4bcec6161208520dd60de5dd2dd5c70ba2577
LockonF <zurielovsko@gmail.com>
2016-02-15 21:59:14 -0600

    Corregidos errores de sintaxis que no permitían que el proyecto se buildeara correctamente

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,7 +4,7 @@
     angular
         .module('app', [
             'triangular','flow','ngFileUpload','ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngMaterial',
-            'ui.router','pascalprecht.translate', 'LocalStorageModule', 'googlechart', 'chart.js', 'linkify', 'ui.calendar', 'angularMoment', 'textAngular', 'uiGmapgoogle-maps', 'hljs', 'md.data.table', angularDragula(angular),
+            'ui.router','pascalprecht.translate', 'LocalStorageModule', 'googlechart', 'chart.js', 'linkify', 'ui.calendar', 'angularMoment', 'textAngular', 'uiGmapgoogle-maps', 'hljs', 'md.data.table', //angularDragula(angular),
             'restangular','toastr','ngMdIcons','oitozero.ngSweetAlert',
             //Gant modules
             'gantt', // angular-gantt.
---
+++ test/spec/index.controller.test.js
@@ -0,0 +1,44 @@
+/**
+ * Created by lockonDaniel on 2/15/16.
+ */
+describe('Proyecto',function(){
+
+    describe('Testing Index',function(){
+        var IndexController, $scope, $httpBackend, Restangular,$mdThemingProvider;
+
+
+
+
+        beforeEach(function(){
+            module('app');
+        });
+
+
+
+        beforeEach(inject(function($controller, $rootScope, $filter, $injector, Restangular, $httpBackend){
+            $scope = $rootScope.$new();
+            $httpBackend = $httpBackend;
+            Restangular = $injector.get("Restangular");
+            IndexController = $controller("indexProyectosController",{
+                $rootScope: $rootScope,
+                $scope: $scope,
+                $filter: $filter
+            });
+
+        }));
+
+        it('Should clear vm.clickedProjects when clear() is called',inject(function(){
+            IndexController.clear();
+            expect(IndexController.clickedProjects).toBe(null);
+        }));
+
+
+
+
+    });
+
+
+
+
+
+});
