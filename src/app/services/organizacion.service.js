5d198995554f190216b718599e049a90f2801f8d
Christian Adan Israel <amezcua9205@gmail.com>
2016-02-21 22:13:30 -0600

    Se realizo la vsta para validar los documentos de una organización

--- src/app/services/organizacion.service.js
+++ src/app/services/organizacion.service.js
@@ -9,10 +9,29 @@
     function Organizacion($q, Restangular) {
         var service = {
             getOrganizacionWitoutValidate: getOrganizacionWitoutValidate,
-            validateOrganizaciones:validateOrganizaciones
+            validateOrganizaciones:validateOrganizaciones,
+            getOrganizacioSinValDoc:getOrganizacioSinValDoc,
+            validateDocument:validateDocument
 
         };
-
+        function validateDocument(id,request){
+            var deferred = $q.defer();
+            Restangular.all('Supervisor').all('Organizacion').all(id).customPUT(request).then(function (res) {
+                deferred.resolve(res);
+            }).catch(function (err) {
+                deferred.reject(err);
+            });
+            return deferred.promise;
+        }
+        function getOrganizacioSinValDoc(){
+            var deferred = $q.defer();
+            Restangular.all('Supervisor').all('Organizacion').all('Documentos').customGET().then(function (res) {
+                deferred.resolve(res.Organizacion);
+            }).catch(function (err) {
+                deferred.reject(err);
+            });
+            return deferred.promise;
+        }
 
         function getOrganizacionWitoutValidate() {
             var deferred = $q.defer();
