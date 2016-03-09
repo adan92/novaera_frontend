f47f9a5a52593faa397e97b456a2ac6da60d93cf
Christian Adan Israel Amezcua <camezcua@mimoni.com>
2016-03-09 17:51:48 -0600

    Faltaron agregar archivos de inscribir proyecto

--- src/app/config.constants.js
+++ src/app/config.constants.js
@@ -5,13 +5,13 @@
     .module('app')
     .constant('ROUTES', {
         //URL-Producción
-        //FILE_ROUTE: 'http://104.197.70.163/',
-        //API_ROUTE: 'http://104.197.70.163/api/'
+        FILE_ROUTE: 'http://104.197.70.163/',
+        API_ROUTE: 'http://104.197.70.163/api/'
         //Local
         //FILE_ROUTE:'http://127.0.0.1:8888/novaera_laravel/public/',
         //API_ROUTE: 'http://127.0.0.1:8888/novaera_laravel/public/api/'
         //Vagrant
-        FILE_ROUTE: 'http://192.168.10.10/',
-        API_ROUTE: 'http://192.168.10.10/api/'
-    }); 
-})(); 
+        //FILE_ROUTE: 'http://192.168.10.10/',
+        //API_ROUTE: 'http://192.168.10.10/api/'
+    });
+})();
