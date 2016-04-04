600658b1fa692d2a971d4d12ca280c7bd2cc0e3c
LockonF <zurielovsko@gmail.com>
2016-04-04 18:55:42 -0500

    Fixes de Pantallas

--- src/app/config.constants.js
+++ src/app/config.constants.js
@@ -5,11 +5,11 @@
     .module('app')
     .constant('ROUTES', {
         //URL-Producción
-        //FILE_ROUTE: 'http://www.novaera-projects.org/laravel/',
-        //API_ROUTE: 'http://www.novaera-projects.org/laravel/api/'
+        FILE_ROUTE: 'http://www.novaera-projects.org/laravel/',
+        API_ROUTE: 'http://www.novaera-projects.org/laravel/api/'
         //FILE_ROUTE: 'http://www.novaera-projects.org/laravel/',
         //API_ROUTE: 'http://www.novaera-projects.org/laravel/api'
-        FILE_ROUTE: 'http://192.168.10.10/',
-        API_ROUTE: 'http://192.168.10.10/api/'
+        //FILE_ROUTE: 'http://192.168.10.10/',
+        //API_ROUTE: 'http://192.168.10.10/api/'
     }); 
 })(); 
